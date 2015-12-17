5f6741d596ecdd5b3dd2ff6fa32b6bec801035b8
bobyangchen <Bob@eduroam-242-090.eng.cam.ac.uk>
2015-12-17 12:04:28 +0000

    added 4 more FireSimulationX files, 5 and 6 are for computing steps, 7 and 8 are for computing survival rates, 5 and 7 are with the door between library and language unit, 6 and 8 are without

--- main.m
+++ main.m
@@ -5,7 +5,11 @@ close all;
 % fireSimulation = FireSimulation();
 % fireSimulation = FireSimulation2();
 % fireSimulation = FireSimulation3();  % as a demo of dynamic programming of the cost, with labels of doors and rooms, only 1 vehicle with info is used
-fireSimulation = FireSimulation4(); % as a demo of message passing, with labels of doors and rooms, only 5 vehicle with info is used, pseudo random seed used
+% fireSimulation = FireSimulation4(); % as a demo of message passing, with labels of doors and rooms, only 5 vehicle with info is used, pseudo random seed used
+% fireSimulation = FireSimulation5(); % run evacuation simulation in baker building 10 times (no fire), and compute the average, with door between library and language unit
+% fireSimulation = FireSimulation6(); % run evacuation simulation in baker building 10 times (no fire), and compute the average, without door between library and language unit
+% fireSimulation = FireSimulation7(); % run evacuation simulation in baker building 10 times (no fire), and compute the average, with door between library and language unit, this script sets health points to 30
+fireSimulation = FireSimulation8(); % run evacuation simulation in baker building 10 times (with fire), and compute the average, without door between library and language unit, this script sets health points to 30
 % fireSimulation = BakerFirstFloor();
 
 fireSimulation.run();
