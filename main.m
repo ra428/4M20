b44546698530b0801f2eea8ae34757714b875758
bobyangchen <Bob@eduroam-241-202.eng.cam.ac.uk>
2015-12-18 12:20:27 +0000

    changed description of firesimulation 7 in main.m

--- main.m
+++ main.m
@@ -8,7 +8,7 @@ close all;
 % fireSimulation = FireSimulation4(); % as a demo of message passing, with labels of doors and rooms, only 5 vehicle with info is used, pseudo random seed used
 % fireSimulation = FireSimulation5(); % run evacuation simulation in baker building 10 times (no fire), and compute the average, with door between library and language unit
 % fireSimulation = FireSimulation6(); % run evacuation simulation in baker building 10 times (no fire), and compute the average, without door between library and language unit
-% fireSimulation = FireSimulation7(); % run evacuation simulation in baker building 10 times (no fire), and compute the average, with door between library and language unit, this script sets health points to 30
+% fireSimulation = FireSimulation7(); % run evacuation simulation in baker building 10 times (with fire), and compute the average, with door between library and language unit, this script sets health points to 30
 fireSimulation = FireSimulation8(); % run evacuation simulation in baker building 10 times (with fire), and compute the average, without door between library and language unit, this script sets health points to 30
 % fireSimulation = BakerFirstFloor();
 
